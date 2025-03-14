df080cc6edff74bfc413ae31b8b98d59592c074c
Brad Hilton <brad.hilton.nw@gmail.com>
2025-03-14 10:39:43 -0600

    chore: Update abstract/rename

--- src/art/interface-snapshot.py
+++ src/art/interface-snapshot.py
@@ -1,12 +1,61 @@
-import httpx
+from contextlib import asynccontextmanager
+from openai.types.chat.chat_completion import Choice
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+from openai.types.chat.chat_completion_tool_param import ChatCompletionToolParam
+from openai import AsyncOpenAI
+from typing import AsyncGenerator, Iterable, Literal
 
-from .model import Model
-from .trajectory import Trajectory
-from .types import BaseModel, TuneConfig, Verbosity
+
+BaseModel = Literal[
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
+    "NousResearch/Hermes-2-Theta-Llama-3-8B",
+    "NousResearch/Hermes-3-Llama-3.1-8B",
+    "Qwen/Qwen2.5-14B-Instruct",
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B",
+    "Qwen/Qwen2.5-32B-Instruct",
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+    "unsloth/Llama-3.3-70B-Instruct",
+]
+
+Message = ChatCompletionMessageParam
+MessageOrChoice = Message | Choice
+Messages = list[Message]
+MessagesAndChoices = list[MessageOrChoice]
+Tools = Iterable[ChatCompletionToolParam]
+
+
+class Trajectory:
+    messages_and_choices: MessagesAndChoices
+    reward: float
+    metrics: dict[str, float]
+    tools: Tools | None
+
+
+class TuneConfig:
+    # GRPO params
+    clip_epsilon: float = 0.2
+    entropy_coef: float = 0.0
+    kl_coef: float = 0.0
+    tanh: bool = False
+
+    # Optimizer params
+    lr: float = 6e-6
+    betas: tuple[float, float] = (0.9, 0.99)
+    weight_decay: float = 0.1
+
+    # Tensor packing params
+    sequence_length: int = 16_384
+
+    # Logging params
+    plot_tensors: bool = False
+    verbosity: "Verbosity" = 1
+
+
+Verbosity = Literal[0, 1, 2]
 
 
 class API:
-    async def get_or_create_model(self, name: str, base_model: BaseModel) -> Model:
+    async def get_or_create_model(self, name: str, base_model: BaseModel) -> "Model":
         """
         Retrieves an existing model or creates a new one.
 
@@ -40,11 +89,13 @@ class Model:
             AsyncOpenAI: An asynchronous OpenAI client.
 
         Example:
+            ```python
             async with model.openai_client() as client:
                 chat_completion = await client.chat.completions.create(
                     model=model.name,
                     messages=[{"role": "user", "content": "Hello, world!"}],
                 )
+            ```
         """
         ...
 
