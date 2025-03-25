f4ff44ade99aa263cee7664d5e72ffd08bc47994
Brad Hilton <brad.hilton.nw@gmail.com>
2025-03-25 17:56:05 -0600

    refactor: Move AsyncOpenAI subclass to its own file

--- src/art/__init__.py
+++ src/art/__init__.py
@@ -29,13 +29,11 @@ from .api import API
 from .gather_groups import gather_groups
 from .local import LocalAPI
 from .model import Model
-from .openai import AsyncOpenAI
 from .types import Messages, MessagesAndChoices, ToolCall, Tools, Trajectory, TuneConfig
 from .unsloth import UnslothAPI
 
 __all__ = [
     "API",
-    "AsyncOpenAI",
     "gather_groups",
     "LocalAPI",
     "Messages",
