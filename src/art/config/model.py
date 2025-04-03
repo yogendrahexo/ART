91fa2d182fb41f2d040f3560172a44a095630446
arcticfly <davidlcorbitt@gmail.com>
2025-04-03 23:54:08 +0000

    Decrease number of scheduler steps

--- src/art/config/model.py
+++ src/art/config/model.py
@@ -34,7 +34,7 @@ def get_model_config(
         enable_prefix_caching=True,
         gpu_memory_utilization=0.6,  # Reduce if out of memory
         max_lora_rank=8,
-        num_scheduler_steps=16,
+        num_scheduler_steps=1,
         use_async=True,
     )
     init_args.update(base_model_config.get("init_args", {}))
