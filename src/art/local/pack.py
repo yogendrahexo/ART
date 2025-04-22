2674d548f5787f8a019281bc97cf0dfcbdd20287
Brad Hilton <brad.hilton.nw@gmail.com>
2025-04-21 20:33:59 -0600

    fix: Address division by zero error

--- src/art/local/pack.py
+++ src/art/local/pack.py
@@ -96,7 +96,9 @@ def packed_tensors_from_tokenized_results(
         assistant_mask[-1].extend(result.assistant_mask)
         logprobs[-1].extend(result.logprobs)
         advantages[-1].extend([result.advantage] * len(result.token_ids))
-        weights[-1].extend([1 / sum(result.assistant_mask)] * len(result.token_ids))
+        weights[-1].extend(
+            [1 / (sum(result.assistant_mask) + 1e-6)] * len(result.token_ids)
+        )
         if truncate_long_results:
             token_ids[-1] = token_ids[-1][:seq_len]
             group_ids[-1] = group_ids[-1][:seq_len]
